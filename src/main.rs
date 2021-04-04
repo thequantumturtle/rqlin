f2d8ae65f2f3d2d44c4d3362a594327d759f9924
thequantumturtle <quantumturtle09@gmail.com>
2021-04-03 23:28:08 -0400

    incorporates basic functions for modulus, conjugate, subtraction and division. Does not handle edge cases

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,21 @@ fn cadd(a:&ComplexNumber, b:&ComplexNumber) -> ComplexNumber{
     cnum(real, imaginary)
 }
 
-fn imul(a:&ComplexNumber, b:&ComplexNumber) -> ComplexNumber{
+fn csub(a:&ComplexNumber, b:&ComplexNumber) -> ComplexNumber{
+    let neg_b = cnum(-b.real, -b.imaginary);
+    cadd(&a, &neg_b)
+}
+
+fn cmul(a:&ComplexNumber, b:&ComplexNumber) -> ComplexNumber{
     let real = a.real * b.real + a.imaginary * b.imaginary * -1.0;
     let imaginary = a.real * b.imaginary + a.imaginary * b.real;
+    cnum(real, imaginary)
+}
+
+fn cdiv(a:&ComplexNumber, b:&ComplexNumber) -> ComplexNumber{
+    let quotient = modulus(&b).powf(2.0);
+    let real = (a.real * b.real + a.imaginary * b.imaginary)/quotient;
+    let imaginary = (b.real * a.imaginary - a.real * b.imaginary)/quotient;
     cnum(real, imaginary)
 }
 
@@ -31,12 +43,23 @@ fn cnum_to_string(c_num:&ComplexNumber) -> String{
     }
 }
 
+fn modulus(c_num:&ComplexNumber) -> f32{
+    (c_num.real.powf(2.0) + c_num.imaginary.powf(2.0)).sqrt()
+}
+
+fn conjugate(c:&ComplexNumber) -> ComplexNumber{
+    cnum(c.real, -c.imaginary)
+}
 fn main() {
-    let a = cnum(3.0, -1.0);
-    let b = cnum(1.0, 4.0);
+    let a = cnum(-2.0, 1.0);
+    let b = cnum(1.0, 2.0);
     let c = cadd(&a, &b);
-    let d = imul(&a, &b);
+    let d = cmul(&a, &b);
     println!("{} PLUS {} Equals {}", cnum_to_string(&a), cnum_to_string(&b),  cnum_to_string(&c));
     println!("{} TIMES {} Equals {}", cnum_to_string(&a), cnum_to_string(&b),  cnum_to_string(&d));
-
+    let c = modulus(&b);
+    println!("The modulus of {} is {}", cnum_to_string(&b), c);
+    println!("The conjugate of {} is {}", cnum_to_string(&a), cnum_to_string(&conjugate(&a)));
+    println!("{} MINUS {} is {}", cnum_to_string(&a), cnum_to_string(&b), cnum_to_string(&csub(&a, &b)));
+    println!("{} DIVIDED BY {} is {}", cnum_to_string(&a), cnum_to_string(&b), cnum_to_string(&cdiv(&a,&b)));
 }
